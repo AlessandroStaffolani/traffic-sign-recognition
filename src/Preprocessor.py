2a02e101cac46c884723ff09ee5bc6b49f271a14
Alessandro Staffolani <alestam93@gmail.com>
2019-01-29 23:03:07 +0100

    Bug Fixes #1

--- src/Preprocessor.py
+++ src/Preprocessor.py
@@ -1,108 +1,123 @@
-import cv2
+import logging
 from os import listdir
 import pandas as pd
-from src.utility.image_utility import load_image
+from src.utility.image_utility import load_image, resize_image
 from src.utility.dataset_utility import get_image_label
+
+logging.basicConfig(filename='log/preprocessor.log', level=logging.INFO,
+                    format='%(levelname)s: %(asctime)s: %(message)s')
 
 
 class Preprocessor:
 
-    def __init__(self, save_path, image_shape=46, training=True, labels=None, image_ext='ppm'):
+    def __init__(self, save_path, image_shape=46, labels=None, image_ext='ppm'):
         """
         Image shape should be a squared image
-        :param save_path:
-        :param image_shape:
-        :param training:
-        :param labels:
-        :param image_ext:
+        :param save_path: path where will be saved the output of the preprocessor
+        :param image_shape: size at which each image will be resized
+        :param labels: list containing all the possible labels for the images
+        :param image_ext: image file extension
         """
-        self.data_folder = None
-        self.image_shape = image_shape
-        self.images_to_process = list()
-        self.images_processed = 0
-        self.is_training = training
         self.save_path = save_path
-        self.processed = None
+        self.image_shape = image_shape
         self.labels = labels
         self.image_ext = image_ext
-        self.current_label = None
+
+        self.data_folder = None  # Folder containing the images to process
+
+        self.images_to_process = list()  # List with the file name of all the images to process
+        self.images_processed = 0  # Index of the current image to process
+
+        self.dataframe = None  # Will contain the dataframe object with the images resized and their label value
+
+        self.current_label = None  # Current folder or label that we are processing
+
+        self.total_images_processed = 0
 
     def init(self):
+        logging.debug('init called')
         if self.data_folder is None:
+            print('data_folder attribute is None! Should be a string')
+            logging.warning('data_folder attribute is None')
             return False
 
         try:
+            # Get all the images from data_folder
             self.images_to_process = listdir(self.data_folder)
         except FileNotFoundError:
             print('Folder "' + self.data_folder + ' not found')
+            logging.error('Folder "' + self.data_folder + ' not found')
             return False
 
+        # Sort the image names and filter to only those with image_ext in the name
         self.images_to_process.sort()
         self.images_to_process = list(filter(lambda file: self.image_ext in file, self.images_to_process))
-        if self.is_training is True:
-            columns = ['image'] + self.labels
-        else:
-            columns = ['image']
 
-        # try:
-        #     self.processed = pd.read_csv(self.save_path, header=None, names=columns)
-        #     self.images_processed = len(self.processed)
-        # except FileNotFoundError:
-        #     self.images_processed = 0
+        # Create the list with all the columns of the dataframe that will be saved on csv
+        columns = ['image'] + self.labels
 
-        self.processed = pd.DataFrame(columns=columns)
+        self.dataframe = pd.DataFrame(columns=columns)
 
         return True
 
+    def clean(self):
+        logging.debug('clean called')
+        self.data_folder = None
+        self.images_to_process = list()
+        self.images_processed = 0
+        del self.dataframe
+        self.dataframe = None
+        self.current_label = None
+
     def set_data_folder(self, data_folder):
         self.data_folder = data_folder
+        logging.info('setted new data_folder: ' + str(self.data_folder))
 
-    def resize_image(self, image):
-        return cv2.resize(image, (self.image_shape, self.image_shape))
+    def set_current_label(self, label):
+        self.current_label = label
+        logging.info('setted new current_label: ' + str(self.current_label))
 
     def status(self):
         return {
             'image_to_process': len(self.images_to_process),
             'image_processed': self.images_processed,
             'percentage': (100 * self.images_processed) / len(self.images_to_process),
-            'next': self.get_next()
+            'next': self.get_next(),
+            'total_processed': self.total_images_processed
         }
 
     def get_next(self):
         if self.images_processed >= len(self.images_to_process):
-            return None
+            image = None
         else:
-            return self.images_to_process[self.images_processed]
-
-    def set_current_label(self, label):
-        self.current_label = label
+            image = self.images_to_process[self.images_processed]
+        logging.debug('get_next result: ' + str(image))
+        return image
 
     def process_next(self):
         next_image_name = self.get_next()
         if next_image_name is None or self.current_label is None:
+            logging.warning('next_image_name is None or current_label is None')
             return False
         else:
             # load image
             next_image = load_image(self.data_folder + '/' + next_image_name)
-            resized = self.resize_image(next_image)
+            resized = resize_image(next_image, (self.image_shape, self.image_shape))
 
-            if self.is_training:
-                # Get label from image name
-                label = get_image_label(self.current_label, self.labels)
-                self.add_to_processed(resized, label)
-            else:
-                self.add_to_processed(resized)
+            label = get_image_label(self.current_label, self.labels)
+            self.add_to_dataframe(resized, label)
 
             # update image processed
             self.images_processed += 1
+            self.total_images_processed += 1
+
+            logging.debug(str(next_image_name) + ' preprocessed and added on dataframe')
 
             return True
 
-    def add_to_processed(self, image, label=None):
-        if label is None:
-            self.processed.loc[self.images_processed] = [image]
-        else:
-            self.processed.loc[self.images_processed] = [image] + label
+    def add_to_dataframe(self, image, label=None):
+        self.dataframe.loc[self.images_processed] = [image] + label
 
     def save_results(self):
-        self.processed.to_csv(index=False, mode='a', path_or_buf=self.save_path, header=False)
+        self.dataframe.to_csv(index=False, mode='a', path_or_buf=self.save_path, header=False)
+        logging.info('saved into ' + str(self.save_path) + ' ' + str(len(self.dataframe)) + ' images')
--- src/controllers/MenuController.py
+++ src/controllers/MenuController.py
@@ -1,8 +1,8 @@
-from os import listdir
-
+from time import time
 from src.Preprocessor import Preprocessor
 from src.utility.system_utility import progress_bar
 from src.utility.dataset_utility import get_labels
+from src.utility.file_utility import get_directory_files, remove_file
 
 
 class MenuController:
@@ -11,7 +11,9 @@ class MenuController:
         self.labels = get_labels(labels_count)
         self.export_file = export_file
         self.image_folder = image_folder
-        self.current_action = 0
+
+        self.current_action = 0  # Action selected by user on the menu
+
         self.preprocessor = Preprocessor(self.export_file, labels=self.labels)
         self._init()
 
@@ -23,24 +25,31 @@ class MenuController:
 
     def handle_menu_action(self):
         if self.current_action == 1:
+            # One label
             label = input('Insert the label to process\n')
             self.preprocess_one_label(self.image_folder + '/' + label, label)
+
         elif self.current_action == 2:
-            # One Label
+            # All label
             folder = input('Insert the folder path (' + self.image_folder + '):\n')
             if folder != '':
                 self.image_folder = folder
 
             self.preprocess_all_images()
-            # self.preprocess_one_label(self.image_folder + '/' + label, label)
+
+        elif self.current_action == 8:
+            # clean log and train
+            self.clean_log_and_train_folder()
+
         elif self.current_action == 9:
+            # Exit
             print("Goodbye")
-            return
         else:
             # No action possible
-            return
+            print("Possible actions are 1, 2, 9")
 
     def preprocess_one_label(self, label_path, label_name):
+        self.preprocessor.clean()
         self.preprocessor.set_data_folder(label_path)
         if not self.preprocessor.init():
             return
@@ -55,15 +64,31 @@ class MenuController:
         self.preprocessor.save_results()
 
     def preprocess_all_images(self):
-        label_names = listdir(self.image_folder)
+        label_names = get_directory_files(self.image_folder)
         label_names.sort()
+        start = time()
         for label in label_names:
             self.preprocess_one_label(self.image_folder + '/' + label, label)
             print('\n')
+        end = time()
+        print('The preprocessor has processed ' + str(self.preprocessor.total_images_processed) + ' images')
+        print('The execution time was ' + str(end - start) + ' seconds')
+
+    def clean_log_and_train_folder(self):
+        log_file = input('Log file (log/preprocessor.log)\n')
+        if log_file == '':
+            log_file = 'log/preprocessor.log'
+        train_file = input('Training output file (data/train_data_processed/train_46x46.csv)\n')
+        if train_file == '':
+            train_file = 'data/train_data_processed/train_46x46.csv'
+        if remove_file(log_file) and remove_file(train_file):
+            print('Clean completed')
 
 
 def print_menu():
     print("\n\nPossible actions: (select the action)", end='\n\n')
-    print('1) Process one class training images', end='\n\n')
-    print('2) Process all training images', end='\n\n')
+    print('1) Process one class training images')
+    print('2) Process all training images')
+    print('\n-- System --------------')
+    print('8) Clean preprocessing log and output folder')
     print('9) Exit', end='\n\n')
