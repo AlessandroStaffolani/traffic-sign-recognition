6d8984d64dd9cdead929fc4b2e36e3dea80805dc
Alessandro Staffolani <alestam93@gmail.com>
2019-03-16 11:06:22 +0100

    Json file now have default config and init project config

--- src/controllers/MultipleRunController.py
+++ src/controllers/MultipleRunController.py
@@ -6,10 +6,10 @@ from src.controllers.MenuController import MenuController, MODELS, ACTIONS
 
 class MultipleRunController:
 
-    def __init__(self, json_path, json_out_path=None):
+    def __init__(self, json_path, json_out_path=None, defualt_config_path='config/default_config.json'):
         self.json_out_path = json_out_path
-        with open(json_path) as file:
-            self.json = json.load(file)
+        self.json = load_json_config(json_path)
+        self.default_config = load_json_config(defualt_config_path)['global']
 
         self.global_config = self.json['global']
         self.instances_config = self.json['instances']
@@ -25,55 +25,84 @@ class MultipleRunController:
         for conf in self.instances_config:
             print('Instance ' + str(count) + ' configs:')
             print('\t' + str(conf))
-            for model in get_property(self.global_config, conf, 'models'):
-                print('Model:\t' + str(MODELS[model]))
-
-                actions = get_property(self.global_config, conf, 'actions')
-                batch_size = get_property(self.global_config, conf, 'size_batch')
-                epochs = get_property(self.global_config, conf, 'epochs')
-                image_shape = get_property(self.global_config, conf, 'image_shape')
-                num_workers = get_property(self.global_config, conf, 'n_workers')
-                model_path = get_property(self.global_config, conf, 'model_path')
-                weights_path = get_property(self.global_config, conf, 'weights_path')
-                color_mode = get_property(self.global_config, conf, 'color_mode')
-                split_factor = get_property(self.global_config, conf, 'split_factor')
-                n_train_samples = get_property(self.global_config, conf, 'n_train_samples')
-                n_validation_samples = get_property(self.global_config, conf, 'n_validation_samples')
-                train_dir = get_property(self.global_config, conf, 'train_dir')
-                validation_dir = get_property(self.global_config, conf, 'validation_dir')
-                test_dir = get_property(self.global_config, conf, 'test_dir')
-
-                menu = MenuController(mode=1,
-                                      actions=actions,
-                                      model=model,
-                                      batch_size=batch_size,
-                                      epochs=epochs,
-                                      image_shape=image_shape,
-                                      num_workers=num_workers,
-                                      model_path=model_path,
-                                      weights_path=weights_path,
-                                      color_mode=color_mode,
-                                      split_factor=split_factor,
-                                      n_train_samples=n_train_samples,
-                                      n_validation_samples=n_validation_samples,
-                                      train_dir=train_dir,
-                                      validation_dir=validation_dir,
-                                      test_dir=test_dir
-                                      )
-                self.stats['stats'].append({
-                    "model": MODELS[model],
-                    "history": menu.history,
-                    "score": menu.scores,
-                    "config": {
-                        "batch_size": batch_size,
-                        "epochs": epochs,
-                        "image_shape": image_shape,
-                        "color_mode": color_mode
-                    }
-                })
+            if 'models' in conf:
+                self.execute_multiple_models(conf)
+            else:
+                self.execute_one_menu(conf)
 
             print('----------------', end='\n\n')
             count += 1
+
+    def execute_multiple_models(self, conf):
+        for model in self.get_property(self.global_config, conf, 'models'):
+            print('Model:\t' + str(MODELS[model]))
+
+            menu_args = self.prepare_menu_args(conf, model)
+
+            menu = MenuController(**menu_args)
+
+            self.stats['stats'].append({
+                "model": MODELS[model],
+                "history": menu.history,
+                "score": menu.scores,
+                "config": {
+                    "batch_size": menu_args['batch_size'],
+                    "epochs": menu_args['epochs'],
+                    "image_shape": menu_args['image_shape'],
+                    "color_mode": menu_args['color_mode']
+                }
+            })
+
+    def execute_one_menu(self, conf):
+
+        model = self.get_property(self.global_config, conf, 'model')
+        menu_args = self.prepare_menu_args(conf, model)
+
+        menu = MenuController(**menu_args)
+
+        if 4 in menu_args['actions'] or 6 in menu_args['actions']:
+            self.stats['stats'].append({
+                "model": MODELS[model],
+                "history": menu.history,
+                "score": menu.scores,
+                "config": {
+                    "batch_size": menu_args['batch_size'],
+                    "epochs": menu_args['epochs'],
+                    "image_shape": menu_args['image_shape'],
+                    "color_mode": menu_args['color_mode']
+                }
+            })
+
+    def prepare_menu_args(self, conf, model):
+        return {
+            "actions": self.get_property(self.global_config, conf, 'actions'),
+            "batch_size": self.get_property(self.global_config, conf, 'batch_size'),
+            "color_mode": self.get_property(self.global_config, conf, 'color_mode'),
+            "epochs": self.get_property(self.global_config, conf, 'epochs'),
+            "image_shape": self.get_property(self.global_config, conf, 'image_shape'),
+            "labels_count": self.get_property(self.global_config, conf, 'labels_count'),
+            "log_folder": self.get_property(self.global_config, conf, 'log_folder'),
+            "mode": 1,
+            "model": model,
+            "model_path": self.get_property(self.global_config, conf, 'model_path'),
+            "n_train_samples": self.get_property(self.global_config, conf, 'n_train_samples'),
+            "n_validation_samples": self.get_property(self.global_config, conf, 'n_validation_samples'),
+            "num_workers": self.get_property(self.global_config, conf, 'num_workers'),
+            "split_factor": self.get_property(self.global_config, conf, 'split_factor'),
+            "test_dir": self.get_property(self.global_config, conf, 'test_dir'),
+            "train_dir": self.get_property(self.global_config, conf, 'train_dir'),
+            "validation_dir": self.get_property(self.global_config, conf, 'validation_dir'),
+            "weights_path": self.get_property(self.global_config, conf, 'weights_path')
+        }
+
+    def get_property(self, global_config, config, key):
+        if key in config:
+            value = config[key]
+        elif key in global_config:
+            value = global_config[key]
+        else:
+            value = self.default_config[key]
+        return value
 
     def save_stats_to_json(self):
         if self.json_out_path is None:
@@ -85,10 +114,7 @@ class MultipleRunController:
             json.dump(self.stats, f)
 
 
-def get_property(global_object, object, key):
-    try:
-        value = object[key]
-    except KeyError:
-        value = global_object[key]
-    return value
-
+def load_json_config(json_path):
+    with open(json_path) as file:
+        json_object = json.load(file)
+    return json_object
